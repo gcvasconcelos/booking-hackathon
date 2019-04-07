cf68dd4d5ff2fedc5bf5658f8a1735ebae9690c9
Gabriel Vasconcelosm <gcvasconcelos98@gmail.com>
2019-04-07 20:26:47 -0300

    Refactored get hotels method and hotel info

--- requests.rb
+++ requests.rb
@@ -1,29 +1,42 @@
 require 'rest-client'
 require 'json'
 
-def get_hotels_facilities_by_country_id(facilities_id, country_id)
+def get_hotels_facilities_by_city_id(city_id)
   offset = 0
-  hotel_ids = []
+  hotels_array = []
   hotels = 1000
 
   while hotels == 1000 do
-    url = "https://distribution-xml.booking.com/2.4/json/hotels?offset=#{offset}&rows=1000&country_ids=#{country_id}&hotel_facility_type_ids=#{facilities_id}"
+    url = "https://distribution-xml.booking.com/2.4/json/hotels?offset=#{offset}&rows=1000&city_ids=#{city_id}&hotel_facility_type_ids=25&extras=hotel_info"
 
     response = RestClient.get(url, headers={'Authorization': "Basic YW5hc2NpbWVudG9zb3V6YTZAZ21haWwuY29tOjYyMTc2MzIwcGVpeGFv"})
     
     hotels_response = JSON.parse(response)
     hotels = hotels_response["result"].length
   
-    hotels_response["result"].each do |hotel|
-      hotel_ids << hotel["hotel_id"]
-    end
+    hotels_array << hotels_response["result"]
     offset += 1000
   end
 
-  hotel_ids
+  hotels_array[0]
 end
 
-def get_hotels_by_country_id(country_id)
+def get_hotel_info(hotel)
+  hotel_info = []
+  hotel_info << url = hotel["hotel_data"]["url"]
+  hotel_info << name = hotel["hotel_data"]["name"]
+  hotel_info << address = hotel["hotel_data"]["address"]
+  hotel_info << location = hotel["hotel_data"]["location"]
+  hotel_info << review_score = hotel["hotel_data"]["review_score"]
+  
+  hotel_info
+end
+
+foo = get_hotels_facilities_by_city_id(-2140479)
+puts get_hotel_info(foo[5])
+
+
+def get_hotels_by_city_id(country_id)
   offset = 0
   hotel_ids = []
   hotels = 1000
@@ -54,18 +67,3 @@ def check_hotel_availability(checkin, checkout, city_id, room_code)
 end
 
 foo = check_hotel_availability("2019-04-08", "2019-04-10", "-1565670", "A,A")
-
-# puts 'Alemanha'
-# foo = get_hotels_by_country_id('de')
-# goo = get_hotels_facilities_by_country_id(25, 'de')
-# puts foo.length + ' ' + goo.length
-
-# puts 'Holanda'
-# foo = get_hotels_by_country_id('nl')
-# goo = get_hotels_facilities_by_country_id(25, 'nl')
-# puts foo.length + ' ' + goo.length
-
-# puts 'Japão'
-# foo = get_hotels_by_country_id('jp')
-# goo = get_hotels_facilities_by_country_id(25, 'jp')
-# puts foo.length + ' ' + goo.length
