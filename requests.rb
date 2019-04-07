b7bf70975ab0b60f16cd627ab4100ddba9cbc401
Gabriel Vasconcelosm <gcvasconcelos98@gmail.com>
2019-04-07 20:35:42 -0300

    Changed method arg to cities

--- requests.rb
+++ requests.rb
@@ -32,25 +32,24 @@ def get_hotel_info(hotel)
   hotel_info
 end
 
-def get_hotels_by_city_id(country_id)
+def get_hotels_by_city_id(city_id)
   offset = 0
-  hotel_ids = []
+  hotels_array = []
   hotels = 1000
 
   while hotels == 1000 do
-    url = "https://distribution-xml.booking.com/2.4/json/hotels?offset=#{offset}&rows=1000&country_ids=#{country_id}"
+    url = "https://distribution-xml.booking.com/2.4/json/hotels?offset=#{offset}&rows=1000&city_ids=#{city_id}"
 
     response = RestClient.get(url, headers={'Authorization': "Basic YW5hc2NpbWVudG9zb3V6YTZAZ21haWwuY29tOjYyMTc2MzIwcGVpeGFv"})
 
     hotels_response = JSON.parse(response)
     hotels = hotels_response["result"].length
 
-    hotels_response["result"].each do |hotel|
-      hotel_ids << hotel["hotel_id"]
-    end
+    hotels_array << hotels_response["result"]
     offset += 1000
   end
-  hotel_ids
+  
+  hotels_array[0]
 end
 
 def check_hotel_availability(checkin, checkout, city_id, room_code)
