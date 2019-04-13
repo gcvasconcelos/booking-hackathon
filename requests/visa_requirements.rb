5e99e715608006a0998ffacd180ed181c90af77b
Gabriel Vasconcelos <gcvasconcelos98@gmail.com>
2019-04-13 14:43:19 -0300

    Changed visa requirements function to accept name

--- requests/visa_requirements.rb
+++ requests/visa_requirements.rb
@@ -1,10 +1,21 @@
 require 'csv'
 load 'country_parse.rb'
 
-def get_visa_requirements(country_id, target_country_id)
+def get_country_id(country_name)
+  file = File.read('resources/countries-data.json')
+  countries = JSON.parse(file)
+
+  country =  countries.find { |country| country['name'] == country_name}
+
+  country["alpha-2"].downcase
+end
+
+def get_visa_requirements(country_name, target_country_id)
   matrix = CSV.read('resources/passport-index-iso2-matrix.csv')
 
   indexes = matrix[0].each { |country| country.downcase! }
+
+  country_id = get_country_id(country_name)
 
   country_id_index = indexes.find_index(country_id)
   target_country_id_index = indexes.find_index(target_country_id)
@@ -22,4 +33,4 @@ def get_visa_requirements(country_id, target_country_id)
   requirements_options[visa_requirements + 1]
 end
 
-# puts get_visa_requirements('br', 'us')
+puts get_visa_requirements('Brazil', 'us')
