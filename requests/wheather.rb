0dc38d90fbc25699d3b1f89ceaab1e76ac4acce0
carlosshaka <carlos.shaka02@gmail.com>
2019-04-13 14:32:11 -0300

    Change file wheather to aws s3

--- requests/wheather.rb
+++ requests/wheather.rb
@@ -1,10 +1,18 @@
+require 'net/http'
 require 'csv'
 
 def get_high_and_low_wheather_by_city_id_and_month(city_id, month)
-  wheather_csv = CSV.read("resources/wheather.csv", headers: true)
+  uri = URI("https://s3.amazonaws.com/dream-team-booking/wheather.csv")
+  csv = Net::HTTP.get_response(uri)
+
+  wheather_csv = CSV.parse(csv.body, headers: true)
   wheather_csv.each do |row|
     if row['city_id'] == city_id && row['month']== month
       return { 'low': row['low'], 'high': row['high'] }
     end 
   end
+  return
 end
+
+# call example
+puts get_high_and_low_wheather_by_city_id_and_month('-2140479', '1')
