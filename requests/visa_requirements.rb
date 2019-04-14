1edbd55cf017e5acf8348efa94e2be2da221d0e1
carlosshaka <carlos.shaka02@gmail.com>
2019-04-14 17:23:25 -0300

    Refactoring visa request

--- requests/visa_requirements.rb
+++ requests/visa_requirements.rb
@@ -1,41 +1,45 @@
-require 'csv'
-require 'rest-client'
+require 'net/http'
 require 'json'
+require 'csv'
+require 'open-uri'
 
-# return iso2 country code by coutry name 
+def main(param)
+ get_visa_requirements(param['country_name'].capitalize, param['target_country_id'])
+end
+
 def get_country_id(country_name)
-  response = RestClient.get('https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.json')
-  countries = JSON.parse(response)
-
-  country =  countries.find { |country| country['name'] == country_name}
-
-  country["alpha-2"].downcase
+ uri = URI('https://s3.amazonaws.com/dream-team-booking/countries.json')
+ json = Net::HTTP.get_response(uri)
+ countries = JSON.parse(json.body)
+ country =  countries.find { |country| country['name'] == country_name}
+   
+ country["alpha-2"].downcase
 end
 
-# return an array with the visa requirement between two countries 
 def get_visa_requirements(country_name, target_country_id)
-  response = RestClient.get('https://s3.amazonaws.com/dream-team-booking/visa.csv')
-  visa_data = CSV.parse(response)
+ uri = URI('https://s3.amazonaws.com/dream-team-booking/visa.csv')
+ csv = Net::HTTP.get_response(uri)
 
-  indexes = visa_data[0].each { |country| country.downcase! }
+ visa_data = CSV.parse(csv.body)
 
-  country_id = get_country_id(country_name)
+ indexes = visa_data[0].each { |country| country.downcase! }
 
-  country_id_index = indexes.find_index(country_id)
-  target_country_id_index = indexes.find_index(target_country_id)
+ country_id = get_country_id(country_name)
 
-  visa_requirements = visa_data[country_id_index+1][target_country_id_index+1].to_i
-  
-  requirements_options = [
-    "for all instances where passport and destination are the same",
-    "visa is required",
-    "visa can be obtained on arrival (which Passport Index considers visa-free)",
-    "eTA is required",
-    "visa-free travel"
-  ]
+ country_id_index = indexes.find_index(country_id)
+ target_country_id_index = indexes.find_index(target_country_id)
 
-  requirements_options[visa_requirements + 1]
-end
-
-# usage example using Brazil-United States visa requirements
-# puts get_visa_requirements('Brazil', 'us')
+ visa_requirements = visa_data[country_id_index+1][target_country_id_index+1].to_i
+ 
+ requirements_options = [
+   "You don't need a visa on domestic travels",
+   "A visa is required to your destination",
+   "Your visa can be obtained on arrival",
+   "An eTA is required on your destination",
+   "Your destination is visa-free"
+ ]
+ 
+ {
+   "visa": requirements_options[visa_requirements+1]
+ }
+end 
